0a983f8a8ca1426f0335e2c012d51107fc37fe80
Na80t <natalia.zavala@ciencias.unam.mx>
2024-05-25 23:30:36 -0600

    Se borro la rama :(

--- Lucylovers/Proyecto.py
+++ Lucylovers/Proyecto.py
@@ -202,3 +202,72 @@ train_loader = DataLoader(train_data, batch_size=4, shuffle=True)
 #Queremos saber las medias de la imagen
 train_features, train_labels = next(iter(train_loader))
 train_features.shape
+
+class Net(nn.Module):
+    def __init__(self):
+        '''
+        Construcción de la Red, define las capas que se utiizaran.
+        '''
+        super(Net, self).__init__()
+
+        # Toma en cuenta el constructor de las capas convolucionales
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1)
+        self.conv2 = nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1)
+        self.conv3 = nn.Conv2d(128, 256, kernel_size=3, stride=1, padding=1)
+        self.pool = nn.MaxPool2d(kernel_size=2, stride=2, padding=0)
+
+        self.fc1 = nn.Linear(256 * 4 * 4, 512)
+        self.fc2 = nn.Linear(512, 256)
+        self.fc3 = nn.Linear(256, 4)
+        self.dropout = nn.Dropout(0.5)
+
+    def forward(self, x):
+        '''
+        Define el orden con el que se realizará la propagación hacia adelante
+        de la red.
+        '''
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = self.pool(F.relu(self.conv3(x)))
+        x = x.view(-1, 256 * 4 * 4)
+        x = F.relu(self.fc1(x))
+        x = self.dropout(x)
+        x = F.relu(self.fc2(x))
+        x = self.dropout(x)
+        x = self.fc3(x)
+        return x
+
+    def custom_train(self, epochs, data_loader, criterion, optimizer, cuda=False):
+        '''
+        Define una función de entrenamiento, teniendo en cuenta la forma en la que llegan
+        los datos, e iteramos sobre estos.
+        '''
+
+        loss_values = []
+
+        if cuda and torch.cuda.is_available():
+            self.cuda()
+
+        for epoch in range(epochs):
+            running_loss = 0.0
+            for i, data in enumerate(data_loader, 0):
+                inputs, labels = data
+                if cuda and torch.cuda.is_available():
+                    inputs, labels = inputs.cuda(), labels.cuda()
+
+                optimizer.zero_grad()
+                outputs = self(inputs)
+                loss = criterion(outputs, labels)
+                loss.backward()
+                optimizer.step()
+                running_loss += loss.item()
+
+            epoch_loss = running_loss / len(data_loader)
+            loss_values.append(epoch_loss)
+            print(f'Epoch {epoch + 1}/{epochs}, Loss: {epoch_loss:.4f}')
+
+        plt.plot(loss_values)
+        plt.xlabel('Epochs')
+        plt.ylabel('Loss')
+        plt.title('Training Loss')
+        plt.show()
